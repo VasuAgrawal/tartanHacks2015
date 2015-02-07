392652b0b6741f9e40393c979f832627e7286fd6
Fletcher Woodruff <fletcherwoodruff@gmail.com>
2015-02-07 00:48:49 -0500

    Post request to start game working

--- site/server.py
+++ site/server.py
@@ -3,15 +3,21 @@ from flask import render_template
 from flask import request
 app = Flask(__name__)
 
+import cardsgame
+
+def create_game(organizers, participants):
+    print organizers
+    print participants
+
 @app.route('/')
 def main():
     return render_template("create.html")
 
 @app.route('/submit', methods=['POST'])
-def create_game():
-   print request.form['organizer']
-   print request.form['participants']
-   return "woo!"
+def submit():
+    create_game(request.form['organizer'], request.form['participants'])
+    return render_template("success.html")
 
 if __name__ == '__main__':
         app.run()
+
