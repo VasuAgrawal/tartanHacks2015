d19cf0861e3d30db01d93afb28ab9abe10577975
Fletcher Woodruff <fletcherwoodruff@gmail.com>
2015-02-07 00:18:11 -0500

    Website taking shape

--- site/server.py
+++ site/server.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template("create.html")
+
+@app.route('/submit', methods=['POST'])
+def create_game():
+   print request.form['organizer']
+   print request.form['participants']
+   return "woo!"
 
 if __name__ == '__main__':
         app.run()
